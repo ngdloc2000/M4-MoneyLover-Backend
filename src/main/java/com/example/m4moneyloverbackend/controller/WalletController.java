24047e8575d1beb6dde6b5949134596bfe11b251
unknown <phamvietcuong12@gmail.com>
2021-12-16 12:46:22 +0700

    wallet

--- src/main/java/com/example/m4moneyloverbackend/controller/WalletController.java
+++ src/main/java/com/example/m4moneyloverbackend/controller/WalletController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+@CrossOrigin("*")
 @Controller
 @RequestMapping("/wallets")
 public class WalletController {
@@ -18,29 +18,6 @@ public class WalletController {
     @GetMapping("/list")
     public ResponseEntity<Iterable<Wallet>>listWallet(){
         return new ResponseEntity<>(walletService.findAll(), HttpStatus.OK);
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<Wallet> createWallet(@RequestBody Wallet wallet){
-        walletService.save(wallet);
-        return new ResponseEntity<>(wallet,HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Wallet> deleteWallet(@PathVariable Long id){
-        walletService.remove(id);
-        return  new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<Wallet> editWallet(@PathVariable Long id, @RequestBody Wallet wallet){
-        Optional<Wallet> walletOptional = walletService.findById(id);
-        if(!walletOptional.isPresent()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        wallet.setId(walletOptional.get().getId());
-        walletService.save(wallet);
-        return new ResponseEntity<>(wallet,HttpStatus.OK);
     }
 }
 
