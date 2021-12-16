7641536cc74ce06c5b4b97e622bc9e41dfe1bec8
dominthao91 <domintaopt@gmail.com>
2021-12-16 13:44:15 +0700

    fix upload file

--- src/main/java/com/example/m4moneyloverbackend/controller/TransactionController.java
+++ src/main/java/com/example/m4moneyloverbackend/controller/TransactionController.java
@@ -2,17 +2,27 @@ package com.example.m4moneyloverbackend.controller;
 
 import com.example.m4moneyloverbackend.model.Transaction;
 import com.example.m4moneyloverbackend.service.transaction.ITransactionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/transactions")
 public class TransactionController {
+    @Autowired
+   private Environment environment;
     @Autowired
     private ITransactionService transactionService;
 
@@ -26,9 +36,23 @@ public class TransactionController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
-        transactionService.save(transaction);
-        return new ResponseEntity<>(transaction, HttpStatus.CREATED);
+    public ResponseEntity<Transaction> createTransaction(@RequestPart("file")MultipartFile file,@RequestPart("newTransaction") String transaction) {
+        MultipartFile multipartFile = file;
+        String file1=multipartFile.getOriginalFilename();
+        try {
+            Transaction transaction1= new ObjectMapper().readValue(transaction,Transaction.class);
+            transaction1.setFile(file1);
+            transactionService.save(transaction1);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        String fileUpload = environment.getProperty("upload.path");
+        try {
+            FileCopyUtils.copy(multipartFile.getBytes(),new File(fileUpload+file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("")
