5b97cdbcf26a97318e70b61fdbeb24d4e89a32de
unknown <phamvietcuong12@gmail.com>
2021-12-17 09:06:30 +0700

    bt

--- src/main/java/com/example/m4moneyloverbackend/repository/transaction/ITransactionRepository.java
+++ src/main/java/com/example/m4moneyloverbackend/repository/transaction/ITransactionRepository.java
@@ -4,6 +4,8 @@ import com.example.m4moneyloverbackend.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface ITransactionRepository extends JpaRepository<Transaction ,Long> {
 }
--- src/main/java/com/example/m4moneyloverbackend/service/transaction/ITransactionService.java
+++ src/main/java/com/example/m4moneyloverbackend/service/transaction/ITransactionService.java
@@ -3,5 +3,7 @@ package com.example.m4moneyloverbackend.service.transaction;
 import com.example.m4moneyloverbackend.model.Transaction;
 import com.example.m4moneyloverbackend.service.IGeneralService;
 
+import java.sql.Date;
+
 public interface ITransactionService extends IGeneralService <Transaction>{
 }
