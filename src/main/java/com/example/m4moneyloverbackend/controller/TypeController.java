a8aaa7198f2f69ddeebacf8249e56491a67fc881
Your Name <you@example.com>
2021-12-19 08:51:13 +0700

    bt

--- src/main/java/com/example/m4moneyloverbackend/controller/TypeController.java
+++ src/main/java/com/example/m4moneyloverbackend/controller/TypeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -44,4 +45,5 @@ public class TypeController {
         typeService.remove(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
